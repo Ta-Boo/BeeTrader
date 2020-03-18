0527db837757cd0c40775da81982fdea4a834962
Tobiáš Hládek <tobias.hladek@avast.com>
2020-03-18 14:30:27 +0100

    taking photo

--- BeeTrader/Controllers/Main/User/Detail/UserDetailViewController.swift
+++ BeeTrader/Controllers/Main/User/Detail/UserDetailViewController.swift
@@ -9,12 +9,27 @@
 import Foundation
 import UIKit
 
-class UserDetailViewController: UITableViewController, UITextFieldDelegate {
+class UserDetailViewController: UITableViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
+    var imagePicker: UIImagePickerController!
+    
+    @IBOutlet weak var avatar: UIImageView!
     
     
     public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+    
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo image: [UIImagePickerController.InfoKey : Any]) {
+        imagePicker.dismiss(animated: true, completion: nil)
+        avatar.image = image[.originalImage] as? UIImage
     }
 }
